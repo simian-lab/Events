c379e3f46cac02f8f23e68ee02e4c53b2071e6d7
Daniel Gómez Didier <dnlgmzddr@gmail.com>
2014-02-23 18:05:33 -0500

    Initial Commit

---
+++ Simian PC/Database/DB.js
@@ -0,0 +1,8 @@
+﻿var lineup = {
+    artist: [
+    {
+        Name: "Los fabulosos",
+        Date: new Date(12, 12, 12, 12, 00, 00),
+        stage: "Lago"
+    }]
+};
---
+++ Simian PC/Scripts/Simian.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="jquery-1.9.1.min.js" />
+/// <reference path="kendo/2013.3.1119/kendo.web.min.js" />
+/// <reference path="../Database/DB.js" />
+$(document).ready(function () {
+    $("#grid").kendoGrid({
+        dataSource: {
+            data: lineup.artist
+        }
+    });
+});
